d99c5861c1b1662c6862dfc7edc18f1f33397c06
MarioCvPz <mariocvpz@gmail.com>
2025-12-05 10:06:27 +0100

    Initial Commit

---
+++ Triviapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Triviapp
+//
+//  Created by Mananas on 5/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
